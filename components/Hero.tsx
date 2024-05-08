8d6e59eb437a6a801078fe494ba0ed3274e3ac69
Efe G <efeasiughu@gmail.com>
2024-05-08 15:22:26 -0400

    done working on header

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,15 +2,15 @@ import VideoPlayer from "./Video/VideoPlayer";
 
 const Hero = () => {
   return (
-    <section id="home" className="pt-30 pb-10 relative h-[95vh] bg-black/50">
-      <div className="absolute top-0 w-full h-full">
+    <section id="home" className="pt-30 pb-10 relative flex flex-col items-start justify-end h-screen bg-black/50">
+      <div className="absolute top-0 w-full h-full overflow-hidden">
         <VideoPlayer
           thumbnailUrl="/images/herothumbnail.png"
           videoUrl="/videos/main-background-video.mp4"
           className="!w-full !h-full !object-contain"
         />
       </div>
-      <div className="contain relative z-10 h-full flex flex-col items-start justify-end">
+      <div className="contain relative z-10 h-[70%] flex flex-col items-start justify-end">
         <h1 className="text-white text-5xl lg:text-[68px] lg:leading-[74.8px] font-normal font-monument">
           The world's first platform for Tokenizing AI blockchain projects
         </h1>
