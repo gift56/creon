d03f63d8fb3a715e9bda4fee5ce9d5443bce22d6
Efe G <efeasiughu@gmail.com>
2024-05-08 16:31:40 -0400

    completing everything

--- components/Profit.tsx
+++ components/Profit.tsx
@@ -1,7 +1,13 @@
-
-
 const Profit = () => {
-  return <section className="bg-black py-14 lg:py-20">Profit</section>;
+  return (
+    <section className="bg-black py-14 lg:py-20">
+      <div className="contain">
+        <div className="w-full flex flex-col lg:flex-row lg:items-center items-start justify-start lg:justify-between gap-8">
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profit;
