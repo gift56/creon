8dea359aaf6d9e2f7ed65f56d3e6344da4b76e4f
Efe G <efeasiughu@gmail.com>
2024-05-08 17:38:51 -0400

    completing everything

--- components/Market.tsx
+++ components/Market.tsx
@@ -13,7 +13,9 @@ const Market = () => {
       </div>
       <div className="w-full h-screen bg-[#13171D99] relative top-0 z-10 flex flex-col gap-5 items-center justify-center">
         {marketData.map((item, index) => (
-          <div key={index} className="w-[90%]"></div>
+          <div key={index} className="w-[90%] bg-[#13171D99] flex flex-col lg:flex-row  items-start justify-start gap-10 lg:items-center lg:justify-between">
+            
+          </div>
         ))}
       </div>
     </section>
