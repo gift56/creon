24f82550d29253902b4ce42a7b472e4e2b790925
Efe G <efeasiughu@gmail.com>
2024-05-08 18:10:14 -0400

    completing everything

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,27 @@
+import { socialIcons } from "@/constant";
+
 const Footer = () => {
   return (
-    <footer className="py-2 bg-black">
+    <footer className="bg-black pb-6">
       <div className="contain">
         <div className="w-full flex flex-col md:flex-row items-center justify-between gap-5">
           <div className="w-full flex items-start justify-start gap-3">
-            <p className="text-base font-normal text-white">© Creon 2023. All rights reserved.</p>
+            <p className="text-base font-normal text-white">
+              © Creon 2023. All rights reserved.
+            </p>
+            <div className="flex items-center justify-start gap-4">
+              {socialIcons.map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-10 h-10 rounded-full border-2 border-white flex items-center justify-center"
+                >
+                  <item.icon size={20} />
+                </Link>
+              ))}
+            </div>
           </div>
           <div></div>
         </div>
--- components/Market.tsx
+++ components/Market.tsx
@@ -12,32 +12,34 @@ const Market = () => {
           <source src="/videos/roadmap-video.mp4" type="video/mp4" />
         </video>
       </div>
-      <div className="w-full relative top-0 z-20 flex flex-col gap-6 items-center justify-center">
-        {marketData.map((item, index) => (
-          <div
-            key={index}
-            className="w-[90%] lg:h-[302px] bg-[#13171D99]  flex flex-col-reverse lg:flex-row  items-start justify-start gap-10 lg:items-center lg:justify-between  rounded-md overflow-hidden"
-          >
-            <div className="flex flex-col px-4 justify-center gap-5 h-full">
-              <h3 className="text-white text-2xl md:text-[31px] biglaptop:text-4xl lg:leading-[45.8px] font-normal font-monument uppercase">
-                {item.title}
-              </h3>
-              <p className="text-lg font-bold font-satoshi text-white">
-                {item.desc}
-              </p>
+      <div className="contain">
+        <div className="w-full relative top-0 z-20 flex flex-col gap-6 items-center justify-center">
+          {marketData.map((item, index) => (
+            <div
+              key={index}
+              className="w-full lg:h-[302px] bg-[#13171D99]  flex flex-col-reverse lg:flex-row  items-start justify-start gap-10 lg:items-center lg:justify-between  rounded-md overflow-hidden"
+            >
+              <div className="flex flex-col px-4 justify-center gap-5 h-full">
+                <h3 className="text-white text-2xl md:text-[31px] biglaptop:text-4xl lg:leading-[45.8px] font-normal font-monument uppercase">
+                  {item.title}
+                </h3>
+                <p className="text-lg font-bold font-satoshi text-white">
+                  {item.desc}
+                </p>
+              </div>
+              <div className="lg:!w-[1551px] ">
+                <Image
+                  src={item.image}
+                  alt="market images"
+                  width={453}
+                  height={304}
+                  priority
+                  className="!w-full !h-[304px] rounded-tl-md rounded-tr-md lg:rounded-tl-none lg:rounded-br-md object-cover"
+                />
+              </div>
             </div>
-            <div>
-              <Image
-                src={item.image}
-                alt="market images"
-                width={453}
-                height={304}
-                priority
-                className="!w-full !h-[304px] lg:!w-[1551px] rounded-tl-md rounded-tr-md lg:rounded-tl-none lg:rounded-br-md"
-              />
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
