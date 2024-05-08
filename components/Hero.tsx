52359d5f82e9bb3483b3f24408a131414d07c361
Efe G <efeasiughu@gmail.com>
2024-05-08 15:51:38 -0400

    working on pass nft section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import VideoPlayer from "./Video/VideoPlayer";
 
 const Hero = () => {
   return (
-    <section id="home" className="min-h-screen lg:pt-30 relative flex flex-col items-start justify-end bg-black/50">
+    <section id="home" className="min-h-screen lg:pt-30 pb-10 relative flex flex-col items-start justify-end bg-black/50">
       <div className="absolute top-0 w-full h-full">
         <VideoPlayer
           thumbnailUrl="/images/herothumbnail.png"
@@ -11,7 +11,7 @@ const Hero = () => {
         />
       </div>
       <div className="contain relative z-10 h-[70%] flex flex-col gap-5 lg:gap-0 items-start justify-end">
-        <h1 className="text-white text-3xl md:text-5xl lg:text-[68px] lg:leading-[74.8px] font-normal font-monument">
+        <h1 className="text-white text-3xl md:text-5xl lg:text-[62px] lg:leading-[74.8px] font-normal font-monument uppercase">
           The world's first platform for Tokenizing AI blockchain projects
         </h1>
         <p className="text-xl font-bold font-satoshi bg-clip-text text-transparent bg-textgradient">Hold the Creon Pass NFT and earn passive income from AI Tools</p>
--- components/PassNft.tsx
+++ components/PassNft.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const PassNft = () => {
+  const passData = [];
+
   return (
     <section id="pass" className="bg-black py-14 lg:py-20">
       <div className="contain">
-        <div className="w-full flex flex-col items-start justify-start"></div>
+        <div className="w-full flex flex-col lg:flex-row items-start justify-start lg:justify-between gap-8">
+          <div className="w-full lg:flex-1 flex flex-col items-start justify-start gap-8">
+            <h2 className="text-white text-3xl md:text-5xl lg:leading-[74.8px] font-normal font-monument uppercase">creon pass nft</h2>
+          </div>
+          <div className="w-full lg:flex-1"></div>
+        </div>
       </div>
     </section>
   );
