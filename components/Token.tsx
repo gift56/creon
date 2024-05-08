2d1be3fab6101078260b9e1aab391e084d998960
Efe G <efeasiughu@gmail.com>
2024-05-08 17:15:45 -0400

    completing everything

--- components/Token.tsx
+++ components/Token.tsx
@@ -1,5 +1,9 @@
 const Token = () => {
-  return <section id="token"  className="bg-black py-14 lg:py-20">Token</section>;
+  return (
+    <section id="token" className="bg-black py-14 lg:py-20">
+      <div className="contain"></div>
+    </section>
+  );
 };
 
 export default Token;
