430d14c28b3399988df4fef68254e84934ee2c99
Efe G <efeasiughu@gmail.com>
2024-05-08 14:46:44 -0400

    done working on header

--- constant/index.ts
+++ constant/index.ts
@@ -11,27 +11,28 @@ export const visionData = [
     image: "/icons/visionIcon2.png",
     title: "Transparent & Fair Decentralized Earnings",
     answer:
-      "At Creon, we handpick cutting-edge AI projects and offer our community and token holders early access and investment opportunities. Our community actively contributes to the growth and profitability of these projects, creating a dynamic ecosystem of innovation and shared success.",
+      "Set to debut in the latter half of 2024, the CREON token serves as the pioneering link between cutting-edge AI initiatives and blockchain technology. This innovative token provides NFT and token holders with unparalleled access to our Launchpad, AI tools, and exclusive pre-launch investment prospects.",
   },
   {
     image: "/icons/visionIcon3.png",
     title: "Profitability and Growth",
     answer:
-      "At Creon, we handpick cutting-edge AI projects and offer our community and token holders early access and investment opportunities. Our community actively contributes to the growth and profitability of these projects, creating a dynamic ecosystem of innovation and shared success.",
+      "CREON NFT and token holders are integral participants in the profit-sharing from our launched AI-based enterprises. We support the development of AI tools, ensuring a steady revenue stream, all while allowing early investors to enjoy the benefits of their support.",
   },
   {
     image: "/icons/visionIcon4.png",
     title: "Profitability and Growth",
     answer:
-      "At Creon, we handpick cutting-edge AI projects and offer our community and token holders early access and investment opportunities. Our community actively contributes to the growth and profitability of these projects, creating a dynamic ecosystem of innovation and shared success.",
+      "The Creon AI Launchpad, an essential component of our initiative, represents a groundbreaking opportunity in the realm of AI and blockchain. It provides the first known platform for acquiring allocations in tokenized AI projects, offering our community of NFT and CREON token holders early access to innovative and promising AI endeavors.",
   },
 ];
 
 export const tokenData = [
   {
     title: "token",
-    desc: "The Gateway token to the world of AI",
-    image: "/images",
+    subtitle: "The Gateway token to the world of AI",
+    image: "/images/tokenImage1.png",
+    desc: "Set to debut in the latter half of 2024, the CREON token serves as the pioneering link between cutting-edge AI initiatives and blockchain technology. This innovative token provides NFT and token holders with unparalleled access to our Launchpad, AI tools, and exclusive pre-launch investment prospects.",
   },
 ];
 
