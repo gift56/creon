176594c47b2f23fef0202423f9217005716eaaaf
Efe G <efeasiughu@gmail.com>
2024-05-08 17:44:00 -0400

    completing everything

--- components/Market.tsx
+++ components/Market.tsx
@@ -1,4 +1,5 @@
 import { marketData } from "@/constant";
+import Image from "next/image";
 
 const Market = () => {
   return (
@@ -15,13 +16,26 @@ const Market = () => {
         {marketData.map((item, index) => (
           <div
             key={index}
-            className="w-[90%] bg-[#13171D99] flex flex-col lg:flex-row  items-start justify-start gap-10 lg:items-center lg:justify-between"
+            className="w-[90%] h-[302px] bg-[#13171D99] flex flex-col-reverse lg:flex-row  items-start justify-start gap-10 lg:items-center lg:justify-between"
           >
-            <div className="flex flex-col px-4 py-5 gap-5">
-              <h3 className="text-white text-2xl md:text-[31px] biglaptop:text-4xl lg:leading-[45.8px] font-normal font-monument uppercase">{item.title}</h3>
-              <p className="text-lg font-bold font-satoshi text-white">{item.desc}</p>
+            <div className="flex flex-col px-4 gap-5 h-full">
+              <h3 className="text-white text-2xl md:text-[31px] biglaptop:text-4xl lg:leading-[45.8px] font-normal font-monument uppercase">
+                {item.title}
+              </h3>
+              <p className="text-lg font-bold font-satoshi text-white">
+                {item.desc}
+              </p>
             </div>
-            <div></div>
+            <div>
+              <Image
+                src={item.image}
+                alt="market images"
+                width={453}
+                height={304}
+                priority
+                className="!w-full !h-[304px] lg:!w-[453px]"
+              />
+            </div>
           </div>
         ))}
       </div>
