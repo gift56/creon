eacc9f157a68b1f048bb0e1ec1f3c577ac857473
Efe G <efeasiughu@gmail.com>
2024-05-08 14:42:09 -0400

    done working on header

--- constant/index.ts
+++ constant/index.ts
@@ -3,6 +3,7 @@ import { FaDiscord, FaTelegramPlane, FaTwitter } from "react-icons/fa";
 export const visionData = [
   {
     image: "/images/visionIcon1.png",
+    title: "Profitability and Growth",
   },
 ];
 
