dc1acfb33591ce3c1a237d637247d7e5003c7179
Efe G <efeasiughu@gmail.com>
2024-05-08 17:17:02 -0400

    completing everything

--- components/Token.tsx
+++ components/Token.tsx
@@ -1,9 +1,13 @@
+import { tokenData } from "@/constant";
+
 const Token = () => {
   return (
     <section id="token" className="bg-black py-14 lg:py-20">
       <div className="contain">
         <div className="w-full grid grid-cols-1 lg:grid-cols-3 items-start justify-start gap-6">
-          
+          {tokenData.map((item, index) => (
+            <div></div>
+          ))}
         </div>
       </div>
     </section>
