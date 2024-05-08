ff2a70e29d17d31179d786e8e352e18a3371f5fa
Efe G <efeasiughu@gmail.com>
2024-05-08 13:51:38 -0400

    working on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,25 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return <header className={`w-full bg-transparent`}>
     
   </header>;
