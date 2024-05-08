d1326a03ce2c0d0571dea0fba5a46fdfb21d3466
Efe G <efeasiughu@gmail.com>
2024-05-08 17:59:51 -0400

    completing everything

--- components/Token.tsx
+++ components/Token.tsx
@@ -11,6 +11,7 @@ const Token = () => {
               key={index}
               className="w-full lg:min-h-[659px] relative bg-[#13171D99] flex flex-col items-start justify-start gap-4 rounded-md"
             >
+              <span className="bg-white rounded-full py-0.5 px-1 absolute -top-2 select-none right-5 text-black text-xs font-bold font-satoshi uppercase">COMING SOON</span>
               <div className="px-4 md:px-5 py-5 rounded-tr-md rounded-tl-md flex flex-col items-start gap-3">
                 <h3 className="text-white text-2xl md:text-[31px] biglaptop:text-4xl lg:leading-[45.8px] font-normal font-monument uppercase">
                   {item.title}
