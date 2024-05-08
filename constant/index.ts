1ae68c29ffc50a7efdcc28d8fe51f2032771566d
Efe G <efeasiughu@gmail.com>
2024-05-08 14:44:10 -0400

    done working on header

--- constant/index.ts
+++ constant/index.ts
@@ -4,6 +4,26 @@ export const visionData = [
   {
     image: "/images/visionIcon1.png",
     title: "Profitability and Growth",
+    answer:
+      "At Creon, we handpick cutting-edge AI projects and offer our community and token holders early access and investment opportunities. Our community actively contributes to the growth and profitability of these projects, creating a dynamic ecosystem of innovation and shared success.",
+  },
+  {
+    image: "/images/visionIcon2.png",
+    title: "Transparent & Fair Decentralized Earnings",
+    answer:
+      "At Creon, we handpick cutting-edge AI projects and offer our community and token holders early access and investment opportunities. Our community actively contributes to the growth and profitability of these projects, creating a dynamic ecosystem of innovation and shared success.",
+  },
+  {
+    image: "/images/visionIcon1.png",
+    title: "Profitability and Growth",
+    answer:
+      "At Creon, we handpick cutting-edge AI projects and offer our community and token holders early access and investment opportunities. Our community actively contributes to the growth and profitability of these projects, creating a dynamic ecosystem of innovation and shared success.",
+  },
+  {
+    image: "/images/visionIcon1.png",
+    title: "Profitability and Growth",
+    answer:
+      "At Creon, we handpick cutting-edge AI projects and offer our community and token holders early access and investment opportunities. Our community actively contributes to the growth and profitability of these projects, creating a dynamic ecosystem of innovation and shared success.",
   },
 ];
 
