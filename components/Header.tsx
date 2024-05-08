aaa242a103b1aabb4be0f9b7e81f3952c34f84ce
Efe G <efeasiughu@gmail.com>
2024-05-08 14:06:49 -0400

    working on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -66,7 +66,7 @@ const Header = () => {
             className="!w-[120px]"
           />
         </Link>
-        <nav>
+        <nav className="flex items-center gap-6">
           {navLinks.map((item, index) => (
             <Link
               href={item.href}
