c08d057f9506b7bf0afa8aeccd1d2848819fe455
Efe G <efeasiughu@gmail.com>
2024-05-08 16:54:21 -0400

    completing everything

--- components/Vision.tsx
+++ components/Vision.tsx
@@ -1,10 +1,21 @@
+"use client";
+import { useState } from "react";
 import VideoPlayer from "./Video/VideoPlayer";
 
 const Vision = () => {
+  const [faqAnswer, setFaqAnswer] = useState(null);
+
+  const handleToggle = (key: any) => {
+    if (faqAnswer === key) {
+      setFaqAnswer(null);
+    } else {
+      setFaqAnswer(key);
+    }
+  };
   return (
     <section id="vision">
       <div className="contain">
-        <div className="w-full flex flex-col lg:flex-row lg:items-center items-start justify-start lg:justify-between gap-8">
+        <div className="w-full flex flex-col lg:flex-row items-start justify-start lg:justify-between gap-8">
           <div className="w-full lg:flex-1">
             <h3 className="text-white text-2xl md:text-[31px] biglaptop:text-4xl lg:leading-[45.8px] font-normal font-monument uppercase">
               Our vision is to support the innovation of AI blockchain projects
