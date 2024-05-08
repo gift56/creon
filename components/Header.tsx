9585de1aaa026ad4532807c3454bcc78f2d11f3c
Efe G <efeasiughu@gmail.com>
2024-05-08 13:31:41 -0400

    working on project itself

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 const Header = () => {
-  return <header className={`w-full bg-transparent`}>Header</header>;
+  return <header className={`w-full bg-transparent`}>
+    
+  </header>;
 };
 
 export default Header;
