6e0def2e000970f1b16b98c1898fedcd233507d6
Efe G <efeasiughu@gmail.com>
2024-05-08 17:16:31 -0400

    completing everything

--- components/Token.tsx
+++ components/Token.tsx
@@ -1,7 +1,11 @@
 const Token = () => {
   return (
     <section id="token" className="bg-black py-14 lg:py-20">
-      <div className="contain"></div>
+      <div className="contain">
+        <div className="w-full grid grid-cols-1 lg:grid-cols-3 items-start justify-start gap-6">
+          
+        </div>
+      </div>
     </section>
   );
 };
