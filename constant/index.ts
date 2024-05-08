8cc76b7f54689a8b0ed7ae7713309fe895523a0e
Efe G <efeasiughu@gmail.com>
2024-05-08 14:47:38 -0400

    done working on header

--- constant/index.ts
+++ constant/index.ts
@@ -34,6 +34,18 @@ export const tokenData = [
     image: "/images/tokenImage1.png",
     desc: "Set to debut in the latter half of 2024, the CREON token serves as the pioneering link between cutting-edge AI initiatives and blockchain technology. This innovative token provides NFT and token holders with unparalleled access to our Launchpad, AI tools, and exclusive pre-launch investment prospects.",
   },
+  {
+    title: "revenue",
+    subtitle: "Driving income and growth through decentralization",
+    image: "/images/tokenImage2.png",
+    desc: "CREON NFT and token holders are integral participants in the profit-sharing from our launched AI-based enterprises. We support the development of AI tools, ensuring a steady revenue stream, all while allowing early investors to enjoy the benefits of their support.",
+  },
+  {
+    title: "launchpad",
+    subtitle: "Driving the future of AI Innovation",
+    image: "/images/tokenImage3.png",
+    desc: "The Creon AI Launchpad, an essential component of our initiative, represents a groundbreaking opportunity in the realm of AI and blockchain. It provides the first known platform for acquiring allocations in tokenized AI projects, offering our community of NFT and CREON token holders early access to innovative and promising AI endeavors.",
+  },
 ];
 
 export const socialIcons = [
