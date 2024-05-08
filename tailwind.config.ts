ce45976c17746acb3dc5371ede59c4ea1609969c
Efe G <efeasiughu@gmail.com>
2024-05-07 20:24:56 -0400

    working on layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: { primary: "#AB23FF" },
       backgroundImage: {
         herogradient:
           "linear-gradient(174.64deg, #000000 4.29%, #3D8BFF 47.39%, #AB23FF 95.05%)",
