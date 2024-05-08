ef9670e471b86fb23de0746a5f142ffd1f973525
Efe G <efeasiughu@gmail.com>
2024-05-08 17:28:00 -0400

    completing everything

--- components/Token.tsx
+++ components/Token.tsx
@@ -9,17 +9,30 @@ const Token = () => {
           {tokenData.map((item, index) => (
             <div
               key={index}
-              className="w-full relative bg-[#13171D99] flex flex-col items-start justify-start gap-4 rounded-md"
+              className="w-full lg:min-h-[659px] relative bg-[#13171D99] flex flex-col items-start justify-start gap-4 rounded-md"
             >
               <div className="px-4 md:px-5 py-5 rounded-tr-md rounded-tl-md flex flex-col items-start gap-3">
                 <h3 className="text-white text-2xl md:text-[31px] biglaptop:text-4xl lg:leading-[45.8px] font-normal font-monument uppercase">
                   {item.title}
                 </h3>
-                <p className="text-lg md:text-xl font-bold font-satoshi bg-clip-text text-transparent bg-textgradient">
+                <p className="text-lg font-bold font-satoshi bg-clip-text text-transparent bg-textgradient">
                   {item.subtitle}
                 </p>
               </div>
-              <Image src={item.image} alt="token images nft" width={900} height={234} priority />
+              <Image
+                src={item.image}
+                alt="token images nft"
+                width={900}
+                height={234}
+                priority
+              />
+              <p className="text-base px-4 md:px-6 font-normal text-white py-4">
+                Set to debut in the latter half of 2024, the CREON token serves
+                as the pioneering link between cutting-edge AI initiatives and
+                blockchain technology. This innovative token provides NFT and
+                token holders with unparalleled access to our Launchpad, AI
+                tools, and exclusive pre-launch investment prospects.
+              </p>
             </div>
           ))}
         </div>
