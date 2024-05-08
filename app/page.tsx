0ea42fb54e6e8aece6994ce9fa824b1b20ab2cb8
Efe G <efeasiughu@gmail.com>
2024-05-08 13:09:06 -0400

    working on project itself

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="text-3xl font-satoshi">
-      Hello
+    <main className="text-3xl">
+      Hello World
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
         textgradient: "linear-gradient(90deg, #3D8BFF 0%, #AB23FF 100%)",
       },
       fontFamily: {
-        satoshi: "Satoshi",
-        monument: "Monument",
+        satoshi: ["Satoshi", "sans-serif"],
+        monument: ["Monument Extended", "sans-serif"],
       },
     },
   },
