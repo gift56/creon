2d590f113e75c4df87eb454c312da9f8cd15e185
Efe G <efeasiughu@gmail.com>
2024-05-08 15:00:31 -0400

    done working on header

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Hero = () => {
-  return <section id="home">Hero</section>;
+  return <section id="home" className="pt-30 pb-10 relative">
+    
+  </section>;
 };
 
 export default Hero;
